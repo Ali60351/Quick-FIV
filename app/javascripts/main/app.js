3a0a84e6e0e0ce11615a081bda61b3eafb60dfa5
Ali Nauman <ali60351@gmail.com>
2018-02-02 19:10:40 +0500

    Added JSON File walk and Hashing functionality

--- app/javascripts/main/app.js
+++ app/javascripts/main/app.js
@@ -1,9 +1,11 @@
 /* jshint esversion: 6 */
 
-var app = require('electron').remote;
+var eApp = require('electron').remote;
 var fs = require('fs');
 var path = require('path');
-// var sqlite3 = require('sqlite3').verbose();
+var hashFiles = require('hash-files');
+var jsonfile = require('jsonfile');
+var file = require('file');
 
 const {
     dialog
@@ -25,24 +27,103 @@ var app = new Vue({
                 properties: ["openDirectory"]
             });
 
-            console.log(dir);
-
             this.selectedDir = dir[0];
 
-            var files = fs.readdirSync(dir[0]);
+            var files = getFiles(this.selectedDir);
 
             for (var i = 0; i < files.length; i++) {
-                //console.log(files[i]);
-                //console.log(fs.lstatSync(files[i]).isDirectory());
-                var location = path.resolve(dir[0], files[i]);
-
-                if (fs.lstatSync(location).isFile()) {
-                    this.queue.push({
-                        name: files[i],
-                        status: 'Pending'
-                    });
-                }
+                this.queue.push({
+                    name: files[i].split(this.selectedDir)[1],
+                    status: 'pending',
+                    path: files[i]
+                });
             }
+
+            setTimeout(function () {
+                var hashArr = getHashes(app.queue);
+                saveJSON(hashArr, path.resolve(app.selectedDir, 'QuickFIV.json'));
+            }, 100);
         }
     }
 });
+
+function getFiles(selection) {
+    var directories = [];
+    var filepaths = [];
+    var call;
+
+    file.walkSync(selection, (callback) => {
+        directories.push(callback);
+    });
+
+    for (var i = 0; i < directories.length; i++) {
+        var files = fs.readdirSync(directories[i]);
+
+        for (var j = 0; j < files.length; j++) {
+            var location = path.resolve(directories[i], files[j]);
+
+            if (fs.lstatSync(location).isFile()) {
+                filepaths.push(location);
+            }
+        }
+    }
+
+    return filepaths;
+}
+
+function getHashes(queue) {
+    var hashes = [];
+
+    for (var i = 0; i < queue.length; i++) {
+        console.log(queue[i].path);
+        var md5 = hashFiles.sync({
+            files: [queue[i].path],
+            algorithm: 'md5',
+            noGlob: true
+        });
+        console.log(md5);
+        var sha1 = hashFiles.sync({
+            files: [queue[i].path],
+            algorithm: 'sha1',
+            noGlob: true
+        });
+        console.log(sha1);
+        var sha256 = hashFiles.sync({
+            files: [queue[i].path],
+            algorithm: 'sha256',
+            noGlob: true
+        });
+        console.log(sha256);
+        var sha512 = hashFiles.sync({
+            files: [queue[i].path],
+            algorithm: 'sha512',
+            noGlob: true
+        });
+        console.log(sha512);
+
+        hashes.push({
+            file: queue[i].name,
+            md5: md5,
+            sha1: sha1,
+            sha256: sha256,
+            sha512: sha512
+        });
+
+        queue[i].status = 'done';
+    }
+
+    return hashes;
+}
+
+function saveJSON(hashArr, location) {
+    fs.writeFileSync(location);
+    jsonfile.writeFileSync(location, hashArr, { spaces: 2 });
+}
+
+// var arr = getFiles('D:\\Music\\C91\\Ambient\\Moon-Tone - Downtime Sessions - Toho Ambient');
+// var arr2 = getHashes(arr);
+// console.log(arr2);
+
+// var file = 'data.json';
+// fs.writeFileSync(file);
+// jsonfile.writeFileSync(file, arr2, {spaces: 2});
